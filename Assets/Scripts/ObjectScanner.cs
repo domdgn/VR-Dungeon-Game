4730233e10d29f4997e5fcbc249fe54dcd581c0e
Kian Wootton <kian200351@outlook.com>
2025-05-05 00:03:15 +0100

    Added Grabbing to test scene and started scanner
    Need to add a canvas/text to create visuals for the scanner, and set the scanner script to activate on trigger not back space

---
+++ Assets/Scripts/ObjectScanner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScanner : MonoBehaviour
+{
+    private ObjectInformation raycastObj;
+    RaycastHit hit;
+      void FixedUpdate()
+    {
+       if(Input.GetKeyDown(KeyCode.Backspace))
+       {
+        Debug.DrawLine(transform.position, transform.forward, Color.red);
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            Debug.Log(hit.transform.gameObject.name);
+            raycastObj = hit.collider.gameObject.GetComponent<ObjectInformation>();
+            var value = raycastObj.GetValue();
+            Debug.Log("Value is " + value );
+        }
+       } 
+    }
+}
