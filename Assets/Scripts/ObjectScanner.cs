db8b26ddc13e34e7411579312d2a919c1d7c55a6
Kian Wootton <kian200351@outlook.com>
2025-05-05 13:25:22 +0100

    scanner

--- Assets/Scripts/ObjectScanner.cs
+++ Assets/Scripts/ObjectScanner.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class ObjectScanner : MonoBehaviour
 {
-    private ObjectInformation raycastObj;
-    RaycastHit hit;
-      void FixedUpdate()
+  private ObjectInformation raycastObj;
+  RaycastHit hit;
+  void FixedUpdate()
+  {
+    Scanner();
+  }
+
+  void Scanner()
     {
-       if(Input.GetKeyDown(KeyCode.Backspace))
-       {
+      if(Input.GetKeyDown(KeyCode.Backspace))
+      {
         Debug.DrawLine(transform.position, transform.forward, Color.red);
         if (Physics.Raycast(transform.position, Vector3.forward, out hit))
         {
-            Debug.Log(hit.transform.gameObject.name);
-            raycastObj = hit.collider.gameObject.GetComponent<ObjectInformation>();
-            var value = raycastObj.GetValue();
-            Debug.Log("Value is " + value );
+          Debug.Log(hit.transform.gameObject.name);
+          raycastObj = hit.collider.gameObject.GetComponent<ObjectInformation>();
+          var value = raycastObj.GetValue();
+          Debug.Log("Value is " + value );
         }
-       } 
+      } 
     }
 }
+
